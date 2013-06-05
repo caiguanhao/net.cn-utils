114c51a87d3f34b6327f1beb043574fec83eed81
caiguanhao <caiguanhao@gmail.com>
2013-06-05 14:57:52 +0800

    add extract archive file;

--- upload.sh
+++ upload.sh
@@ -5,6 +5,7 @@ PWD="`pwd`"
 INFO_SH="info.sh"
 REMOTE_DIR="/htdocs"
 ZIP=$(which zip)
+EXTRACT=0
 
 if [[ ${#CURL} -eq 0 ]]; then
     echo "Install curl first."
@@ -15,6 +16,12 @@ if [[ ${#ZIP} -eq 0 ]]; then
     echo "Install zip first."
     exit 1
 fi
+
+QUERY_URL="http://cp.hichina.com/AJAXPage.aspx"
+
+USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3)"
+USER_AGENT="${USER_AGENT} AppleWebKit/537.31 (KHTML, like Gecko)"
+USER_AGENT="${USER_AGENT} Chrome/26.0.1410.65 Safari/537.31"
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -35,6 +42,10 @@ while [[ $# -gt 0 ]]; do
                 TO="$1"
                 shift
             fi
+            ;;
+        -e|--extract)
+            shift
+            EXTRACT=1
             ;;
         *)
             break
@@ -72,6 +83,8 @@ if [[ ${#TO} -gt 0 ]]; then
         fi
         TO="${TO}${FROM##*/}"
     fi
+else
+    TO=${FROM##*/}
 fi
 if [[ ${TO:0:1} != "/" ]]; then
     TO="/${TO}"
@@ -85,4 +98,48 @@ $CURL --ftp-create-dirs -T "${FROM}" "${FTP}${TO}"
 
 if [[ ${#TMP_FILE} -gt 0 ]]; then
     rm -f "${TMP_FILE}"
+fi
+
+if [[ $EXTRACT -eq 1 ]]; then
+
+    if [[ ${#TO} -eq 0 ]]; then
+        echo "[Error] Fail to extract file: no input file."
+        exit 1
+    fi
+
+    OUTPUT=`$CURL -s -G "http://cp.hichina.com/FileUncompressionold.aspx" \
+    -d "tr=fileuncompressionold" \
+    -b "${PWD}/cookie" \
+    -c "${PWD}/cookie" \
+    -A "${USER_AGENT}" \
+    -o /dev/null \
+    -w "%{http_code}"`
+
+    if [[ $OUTPUT -ne 200 ]]; then
+        echo "[Exception] Exit with status code ${OUTPUT} (should be 200)."
+        if [[ $OUTPUT -eq 302 ]]; then
+            echo "[Exception] You may need to log in again."
+        fi
+        exit 1
+    fi
+
+    echo "${TO} will be extracted to / . Enter to continue. Ctrl-C tp cancel."
+    read
+
+    OUTPUT=`$CURL -s -G "${QUERY_URL}" \
+    -d "action=uncommpressfilesold" \
+    -d "serverfilename=${TO}" \
+    -d "serverdir=/" \
+    -d "iscover=1" \
+    -b "${PWD}/cookie" \
+    -c "${PWD}/cookie" \
+    -A "${USER_AGENT}" | iconv -f gbk`
+
+    if [[ $OUTPUT == *200\|OK* ]]; then
+        echo "[OK] File has been successfully extracted."
+    else
+        echo "[Error] Fail to extract file. Message: $OUTPUT"
+        exit 1
+    fi
+
 fi
