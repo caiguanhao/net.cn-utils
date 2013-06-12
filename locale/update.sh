9297714d85db5f8ffb83fe83862e60ffe95a69ab
caiguanhao <caiguanhao@gmail.com>
2013-06-12 15:19:44 +0800

    auto generate for new langs;

--- locale/update.sh
+++ locale/update.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MAIN="$( dirname "$DIR" )"
 
@@ -17,17 +15,47 @@ SHELL_FILES=(
     $(find "${MAIN}" -maxdepth 1 -type f -name "*.sh")
 )
 
+CHECK()
+{
+    if [[ $? -ne 0 ]]; then
+        echo "Fail"
+        exit 1
+    fi
+}
+
 for SF in "${SHELL_FILES[@]}"; do
     SF_D=${SF%/*}
     SF_F=${SF##*/}
     for LC in "${LOCALES[@]}"; do
         LC_F=${LC##*/}
-        echo -n "Processing $SF_F for $LC_F ..."
-        FILE="${DIR}/${LC_F}/LC_MESSAGES/${SF_F}"
-        cd "$SF_D" && $BASH --dump-po-strings "${SF_F}" > "${FILE}.po2"
-        $MSGUNIQ -o "${FILE}.po2" "${FILE}.po2"
-        $MSGMERGE --backup=none --update "${FILE}.po" "${FILE}.po2"
-        $MSGFMT -o "${FILE}.mo" "${FILE}.po"
-        rm -f "${FILE}.po2"
+        echo -n "Processing $SF_F for $LC_F ... "
+        LC_M="${DIR}/${LC_F}/LC_MESSAGES"
+        FILE="${LC_M}/${SF_F}"
+        if [[ ! -d "$LC_M" ]]; then
+            mkdir "$LC_M"
+        fi
+        if [[ ! -f "${FILE}.po" ]]; then
+            cd "$SF_D" && $BASH --dump-po-strings "${SF_F}" > "${FILE}.po"
+            $MSGUNIQ -o "${FILE}.po" "${FILE}.po" >/dev/null 2>&1
+            CHECK
+            echo 'msgid ""
+msgstr ""
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+' | cat - "${FILE}.po" > "${FILE}.po~" && mv "${FILE}.po~" "${FILE}.po"
+            $MSGFMT -o "${FILE}.mo" "${FILE}.po" >/dev/null 2>&1
+            CHECK
+            echo "Done"
+        else
+            cd "$SF_D" && $BASH --dump-po-strings "${SF_F}" > "${FILE}.po2"
+            $MSGUNIQ -o "${FILE}.po2" "${FILE}.po2" >/dev/null 2>&1
+            CHECK
+            $MSGMERGE --backup=none --update "${FILE}.po" "${FILE}.po2" >/dev/null 2>&1
+            CHECK
+            $MSGFMT -o "${FILE}.mo" "${FILE}.po" >/dev/null 2>&1
+            CHECK
+            rm -f "${FILE}.po2"
+            echo "Done"
+        fi
     done
 done
